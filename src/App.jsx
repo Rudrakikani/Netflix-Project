6b9c1fa2590437b83287463b91d2aa22cc67ad36
rudrakikani <rudrakikani38@gmail.com>
2025-01-10 01:54:19 +0530

    frontend-setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import {Route, Routes } from 'react-router-dom';
-import HomePage from './Pages/HomePage';
-import SignUpPage from './Pages/SignUpPage';
-import LoginPage from './Pages/LoginPage';
+import { Route, Routes } from "react-router-dom";
+import LoginPage from "./Pages/LoginPage";
+import SignUpPage from "./Pages/SignUpPage";
+import HomePage from "./Pages/home/HomePage";
+import Footer from "./components/Footer";
+import { Toaster } from "react-hot-toast";
+import HomeScreen from "./Pages/home/HomeScreen";
 
-function App() {
-
-  return ( 
-    <Routes>
-      <Route path='/' element={<HomePage/>} />
-      <Route path='/Login' element={<LoginPage/>} />
-      <Route path='/SignUp ' element={<SignUpPage/>} />
-    </Routes>
-    
-  
-  );
+export default function App() {
+  return (
+    <>
+      <Toaster />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/HomeScreen" element={<HomeScreen />} />
+      </Routes>
+      <Footer />
+    </>
+  )
 }
-
-export default App
- 
